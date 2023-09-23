4af7aec3d9e0b5ffa20dcb0326c1d2f89b2a8532
erickbauerv <hullbauer@gmail.com>
2023-09-23 13:47:31 -0300

    Implementado o contexto de banco de dados.

---
+++ FilmesAPI/Data/FilmeContext.cs
@@ -0,0 +1,15 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public DbSet<Filme> Filmes { get; set; }
+
+        public FilmeContext(DbContextOptions<FilmeContext> options) : base(options)
+        {
+            
+        }
+    }
+}
--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -1,6 +1,12 @@
+using FilmesAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
 
 // Add services to the container.
+builder.Services.AddDbContext<FilmeContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
